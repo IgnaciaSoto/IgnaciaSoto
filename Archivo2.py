a50db2da1f9302d6d122322fb5554a1492da12da
Ignacia Soto <ignacia.soto.c@mail.pucv.cl>
2020-09-06 21:38:38 -0300

    Archivo2

---
+++ Archivo2.py
@@ -0,0 +1,7 @@
+diccionario={'nombre': 'Ignacia', 'apellido': 'Moya', 'nota programacion': 7.0, 'nota fisica': 6.0}
+diccionario1={'nombre': 'Bayron', 'apellido': 'Tobar', 'nota programacion': 6.0, 'nota fisica': 6.8}
+diccionario2={'nombre': 'Ivannya', 'apellido': 'Cifuentes', 'nota programacion': 5.6, 'nota fisica': 4.7}
+diccionario3={'nombre': 'Ignacia', 'apellido': 'Soto', 'nota programacion': 5.0, 'nota fisica': 4.9}    
+
+print(f'La respuesta a diccionario es {{'nombre': 'Ignacia', 'apellido': 'Moya', 'nota programacion': 7.0, 'nota fisica': 6.0}
+
